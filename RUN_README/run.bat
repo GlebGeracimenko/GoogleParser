4854ec3a02236717669463f621a7ac4c2336dfdc
GlebGeracimenko <gleb.geracimenko@gmail.com>
2015-06-06 22:23:07 +0300

    Full project

--- RUN_README/run.bat
+++ RUN_README/run.bat
@@ -1,6 +1,1 @@
-cd.. &&
-mvn clean &&
-mvn package &&
-cd target/ &&
-java -jar googleParser-1.0-SNAPSHOT-jar-with-dependencies.jar &&
-cmd /k
+cd.. && mvn clean && mvn package && cd target/ && java -jar googleParser-1.0-SNAPSHOT-jar-with-dependencies.jar && cmd /k
