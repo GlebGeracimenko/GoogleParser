6dace7837197000fb369925c7df076ea27f2cdbb
ggerasimenko <ggerasimenko@itteam.io>
2017-01-31 12:41:21 +0200

    fix for all OS

--- src/main/java/com/gleb/Test.java
+++ src/main/java/com/gleb/Test.java
@@ -7,13 +7,29 @@ import java.util.Scanner;
  */
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input search request");
         String key = scanner.nextLine();
-        System.setProperty("webdriver.gecko.driver", "../geckodriver");
+        String os = System.getProperty("os.name").toLowerCase();
+        String folderName = getFolderName(os);
+        if (folderName == null) {
+            throw new Exception("OS is not support");
+        }
+        System.setProperty("webdriver.gecko.driver", "driver/" + folderName + "/geckodriver");
         GoogleParser googleParser = new GoogleParser(key);
         googleParser.start();
+    }
+
+    private static String getFolderName(String os) {
+        if (os.contains("mac")) {
+            return "macOS";
+        } else if (os.contains("windows")) {
+            return "windows";
+        } else if (os.contains("nix") || os.contains("nux")) {
+            return "linux";
+        }
+        return null;
     }
 
 }
